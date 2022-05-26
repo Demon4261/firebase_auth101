6aa5751f6904fa83bf8496efe5e8fc99dddcca9f
Demon4261 <anshmalik279@gmail.com>
2022-05-26 22:08:35 +0530

    Login_method page created

---
+++ lib/screens/login_method_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/my_button.dart';
+
+class LoginMethod extends StatefulWidget {
+  LoginMethod({Key? key}) : super(key: key);
+
+  @override
+  State<LoginMethod> createState() => _LoginMethodState();
+}
+
+class _LoginMethodState extends State<LoginMethod> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Firebase Auth"),
+          elevation: 0.0,
+        ),
+        body: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Center(
+              child: Column(
+            children: const [
+              SizedBox(
+                height: 20,
+              ),
+              MyButtons(
+                inputText: "Login Screen",
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              MyButtons(inputText: "Signup Screen"),
+              SizedBox(
+                height: 20,
+              ),
+              MyButtons(inputText: "Mobile"),
+              SizedBox(
+                height: 20,
+              ),
+              MyButtons(inputText: "Gmail Login"),
+              SizedBox(
+                height: 20,
+              ),
+              MyButtons(inputText: "Fb Login")
+            ],
+          )),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/my_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyButtons extends StatelessWidget {
+  final String inputText;
+
+  const MyButtons({Key? key, required this.inputText}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        height: 50,
+        width: 300,
+        decoration: BoxDecoration(
+            color: Colors.blue, borderRadius: BorderRadius.circular(12)),
+        child: Center(
+            child: Text(
+          inputText,
+          style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              wordSpacing: 3,
+              letterSpacing: 1.5),
+        )),
+      ),
+    );
+  }
+}
