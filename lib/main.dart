4eb12376982a7c74fde0192883009bc151108127
Demon4261 <anshmalik279@gmail.com>
2022-05-28 15:03:24 +0530

    firebase setup completed for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_auth101/firebase_options.dart';
 import 'package:firebase_auth101/screens/login_method_screen.dart';
 import 'package:firebase_auth101/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
